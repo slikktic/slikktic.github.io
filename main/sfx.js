1bd6a34fe4e144bb0a6490544b8d7d1958a4741c
slikktic <emjake037@gmail.com>
2025-06-08 19:32:35 -0400

    bg change, button sound

--- main/sfx.js
+++ main/sfx.js
@@ -9,4 +9,21 @@ document.addEventListener("DOMContentLoaded", () => {
     door.addEventListener("mouseleave", () => {
         doorSound.pause();
     });
+
 });
+
+const tabsound = document.getElementsByClassName("tabsound");
+const linkbutton = document.getElementsByClassName("linkbutton");
+
+function play(audio) {
+    audio.playbackRate = 2; //(Math.random() * (1.5 - 0.6) + 0.6);
+    audio.preservesPitch = false;
+    audio.currentTime = 0;
+    audio.play();
+}
+
+for (let i = 0; i < linkbutton.length; i++) {
+    linkbutton[i].addEventListener("mouseover", () => {
+        play(tabsound[i]);
+    })
+}
